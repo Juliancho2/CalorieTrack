7cdbdd19061586de6fded7c3914752561cae2d2b
julian moreno <julianmoreno242@gmail.com>
2023-11-13 12:12:39 -0500

    [progress] custom hook Async storage

---
+++ src/hooks/useFoodStorage.ts
@@ -0,0 +1,57 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { Meal } from "../types";
+
+const MY_FOOD_KEY = "@MyFood:key";
+
+const useFoodStorage = () => {
+  const handleSaveFood = async ({ calories, name, portion }: Meal) => {
+    try {
+      const currentSaveFood = await AsyncStorage.getItem(MY_FOOD_KEY);
+      if (currentSaveFood !== null) {
+        const currentFoodParsed = JSON.parse(currentSaveFood);
+        currentFoodParsed.push({
+          calories,
+          name,
+          portion,
+        });
+        await AsyncStorage.setItem(
+          MY_FOOD_KEY,
+          JSON.stringify(currentFoodParsed),
+        );
+        return Promise.resolve();
+      }
+      await AsyncStorage.setItem(
+        MY_FOOD_KEY,
+        JSON.stringify({
+          calories,
+          name,
+          portion,
+        }),
+      );
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+  const handleGetFoods = async () => {
+    try {
+      const foods = await AsyncStorage.getItem(MY_FOOD_KEY);
+      if (foods !== null) {
+        const parsedFoods = JSON.parse(foods);
+        return Promise.resolve(parsedFoods);
+      }
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+
+  return {
+    onGetFoods: handleGetFoods,
+    onSaveFood: handleSaveFood,
+  };
+};
+//saving food information
+//it is a function to get food information
+
+export default useFoodStorage;
